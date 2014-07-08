87c9132508c4337f12e07f8cad551c8883c54b9f
theWingThing <thewingthing@gmail.com>
2014-07-07 18:22:56 -0700

    initial commit

---
+++ app/src/main/java/com/happifying/guideme/app/Main.java
@@ -0,0 +1,36 @@
+package com.happifying.guideme.app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nar on 7/7/14.
+ */
+public class Main extends FragmentActivity {
+    MyPageAdapter pageAdapter;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        List<Fragment> fragments = getFragments();
+        pageAdapter = new MyPageAdapter(getSupportFragmentManager(), fragments);
+        ViewPager pager =
+                (ViewPager)findViewById(R.id.viewpager);
+        pager.setAdapter(pageAdapter);
+    }
+
+    private List<Fragment> getFragments() {
+        List<Fragment> fList = new ArrayList<Fragment>();
+
+        fList.add(MyFragment.newInstance("Fragment 1"));
+        fList.add(MyFragment.newInstance("Fragment 2"));
+        fList.add(MyFragment.newInstance("Fragment 3"));
+
+        return fList;
+    }
+}
