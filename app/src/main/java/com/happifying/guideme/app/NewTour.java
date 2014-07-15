abda0b79c7c8b19a6f5fae4b2aa088021b3e5543
theWingThing <thewingthing@gmail.com>
2014-07-14 20:46:41 -0700

    reset view succeeded

--- app/src/main/java/com/happifying/guideme/app/NewTour.java
+++ app/src/main/java/com/happifying/guideme/app/NewTour.java
@@ -2,14 +2,20 @@ package com.happifying.guideme.app;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 
 /**
  * Created by nar on 7/14/14.
  */
 public class NewTour extends ActionBarActivity {
+    int layouts[] = {R.layout.new_where, R.layout.new_attractions}, i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.new_where);
+        setContentView(layouts[i++]);
+    }
+
+    public void onClick(View view) {
+        setContentView(layouts[i++]);
     }
 }
